a7aaf641649c413ef40dd27d3844f50904247e3e
MadsHaerup <69457070+MadsHaerup@users.noreply.github.com>
2021-08-04 19:36:46 +0200

    components

---
+++ src/components/headline/Headline.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Headline() {
+  return (
+    <div style={{display: "flex", flexDirection:"column", textAlign:"center", lineHeight:1}}>
+      <h1 style={{textTransform:"uppercase", margin:0, marginTop:"1rem", fontSize:"3rem"}}>mads</h1>
+      <h1 style={{color: "#FEB300",textTransform:"uppercase", transform: "rotate(180deg", margin:0, fontSize:"3rem"}}>haerup</h1>
+    </div>
+  )
+}
--- src/components/person/Person.js
+++ src/components/person/Person.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import Pb from '../../assets/img/pb.png';
-import {motion} from 'framer-motion';
-import { infinity } from 'check-types';
-
+import {motion, transform} from 'framer-motion';
+import Headline from '../headline/Headline';
 export default function Person() {
   return (
+    <>
+    <Headline/>
     <div
-    style={{display: "grid", width: "400px", margin:"0 auto", position: "relative"}}
+    style={{display: "grid", width: "600px", margin:"0 auto", position: "relative", height: "100vh"}}
     >
-      <motion.img src={Pb} alt="" width="400"
-      style={{position: "absolute", top:0, left:0, zIndex: 100, perspective: "50px",
-      perspectiveOrigin: "left"  }} 
+  
+
+      <motion.img src={Pb} alt="" width="600"
+      style={{position: "absolute", top:0, left:0, zIndex: 100 }} 
       animate={{}}
       />
-      <motion.img src={Pb} alt="" width="400"
+      <motion.img src={Pb} alt="" width="600"
       style={{position: "absolute", top:0, left:0, zIndex: 5}}
       initial={{ opacity: 0, x: -10, }}  
-      animate={{ opacity: 1, y:-2, x:60}}
-      transition={{delay:0.5, duration:0.6,yoyo: Infinity }}
+      animate={{ opacity: 1, y:-2, x:70}}
+      transition={{delay:0.5, duration:1, repeat: Infinity}}
       />
-      <motion.img src={Pb} alt="" width="400"
+      <motion.img src={Pb} alt="" width="600"
       style={{position: "absolute", top:0, left:0, zIndex: 2}}
       initial={{ opacity:0, x:-5}}  
-      animate={{ opacity: 1, y:-4, x:110}}
-      transition={{delay:0.5, duration:0.6, yoyo: Infinity}}
+      animate={{ opacity: 1, y:-4, x:140}}
+      transition={{delay:0.5, duration:1, repeat:Infinity }}
       />
     </div>
+    </>
   )
 }
