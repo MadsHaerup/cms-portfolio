2e833f0668a7634d2e51dd8a7bdba508cb50be82
MadsHaerup <69457070+MadsHaerup@users.noreply.github.com>
2021-10-06 16:21:00 +0200

    new modern feel

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ToTopBtn from "../totopButton/ToTopBtn";
+import { FaGithub, FaLinkedinIn, FaDev } from "react-icons/fa";
+
 import "./Footer.css";
 
 export default function Footer() {
@@ -8,17 +10,20 @@ export default function Footer() {
 			<ul className="footerList">
 				<li className="footerList__item">
 					<a className="footerList__item__Link" href="https://github.com/MadsHaerup">
-						Github
+						<span className="footerList__item__Link__name">Github</span>
+						<FaGithub />
 					</a>
 				</li>
 				<li className="footerList__item">
 					<a className="footerList__item__Link" href="https://www.linkedin.com/in/mads-h%C3%A6rup-24b00a218/">
-						LinkedIn
+						<span className="footerList__item__Link__name">LinkedIn</span>
+						<FaLinkedinIn />
 					</a>
 				</li>
 				<li className="footerList__item">
 					<a className="footerList__item__Link" href="https://dev.to/madshaerup">
-						Dev
+						<span className="footerList__item__Link__name">Dev</span>
+						<FaDev />
 					</a>
 				</li>
 			</ul>
